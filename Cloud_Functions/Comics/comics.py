2b39f19cad7b0942724e27ac92500e797b931d15
John Kellerman <kellrman@us.ibm.com>
2021-03-22 20:38:05 -0400

    Finish off comics with deploy

--- Cloud_Functions/Comics/comics.py
+++ Cloud_Functions/Comics/comics.py
@@ -7,31 +7,78 @@ import os
 import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-import json
+import sys
+import base64
+
+GOCOMICS = 'gocomics'
+COMICSKINGDOM = 'comicskingdom'
+DILBERT = 'dilbert'
+FARSIDE = 'farside'
 
 GOCOMICS_URL = 'https://www.gocomics.com/'
-IMAGE_URL_REGEX = r'(\S+item-comic-image.+data-srcset=\")(\S+)'
-SSL_PORT = 465  # For SSL
+GOCOMICS_REGEX = r'(\S+item-comic-image.+data-srcset=\")(\S+)'
+COMICSKINGDOM_URL = 'https://www.comicskingdom.com/'
+COMICSKINGDOM_REGEX = r'(og:image\"\s+content=\")([^\"]+)'
+DILBERT_URL = 'https://dilbert.com/'
+DILBERT_REGEX = r'(data-image=\")([^\"]+)'
+FARSIDE_URL = 'https://www.thefarside.com'
+FARSIDE_REGEX = r'(.+data-src=\")([^\"]+)'
 
-def process_comic(comic_name, today_date_string, processing_results):
-    url = "%s%s/%s" % (GOCOMICS_URL, comic_name, today_date_string)
-    response = requests.get(url)
+EMAIL_SSL_PORT = 465  # For SSL
+
+COMICS = [
+    {"name": "luann", "source": GOCOMICS},
+    {"name": "dilbert", "source": DILBERT},
+    {"name": "dilbert-classics", "source": GOCOMICS},
+    {"name": "zits", "source": COMICSKINGDOM},
+    {"name": "baby-blues", "source": COMICSKINGDOM},
+    {"name": "wallace-the-brave", "source": GOCOMICS},
+    {"name": "frazz", "source": GOCOMICS},
+    {"name": "bizarro", "source": COMICSKINGDOM},
+    {"name": "theargylesweater", "source": GOCOMICS},
+    {"name": "nonsequitur", "source": GOCOMICS},
+    {"name": "calvinandhobbes", "source": GOCOMICS},
+    {"name": "bloomcounty", "source": GOCOMICS},
+    {"name": "outland", "source": GOCOMICS},
+    {"name": "getfuzzy", "source": GOCOMICS},
+    {"name": "theflyingmccoys", "source": GOCOMICS},
+    {"name": "closetohome", "source": GOCOMICS},
+    {"name": "boundandgagged", "source": GOCOMICS},
+    {"name": "doonesbury", "source": GOCOMICS},
+    {"name": "academiawaltz", "source": GOCOMICS},
+    {"name": "prince-valiant", "source": COMICSKINGDOM},
+    {"name": "farside", "source": FARSIDE}
+]
+
+def get_image_url(comic_url, comic_regex):
+    # This assumes the regex returns the desired image url in group(2) of the regex match
+    image_url = None
+    # verify=False is to eliminate the error, requests.exceptions.SSLError: [SSL: CERTIFICATE_VERIFY_FAILED]
+    response = requests.get(comic_url, verify=False)
     if response.status_code == 200:
         results = response.content.decode('utf-8')
-        regex_match = re.search(IMAGE_URL_REGEX, results)
-        if regex_match is not None:
+        regex_match = re.search(comic_regex, results)
+        if regex_match is not None and len(regex_match.groups()) >= 2:
             image_url = regex_match.group(2)
-            if image_url is not None:
-                new_entry = {'name': comic_name, 'image_url': image_url, 'gocomics_url': url}
-                processing_results.append(new_entry)
-                print(new_entry)
-            else:
-                print('Comic %s not found for %s.' % (comic_name, today_date_string))
-        else:
-            print('Comic %s not found for %s.' % (comic_name, today_date_string))
     else:
-        print('%s error calling gocomics.com: %s' % (response.status_code, response.content))
-    return
+        print('%s error calling %s: %s' % (response.status_code, comic_url, response.content))
+    return image_url
+
+def get_image(comic_url,comic_regex):
+    image = None
+    response = requests.get(comic_url, verify=False)
+    if response.status_code == 200:
+        results = response.content.decode('utf-8')
+        regex_match = re.search(comic_regex, results)
+        if regex_match is not None and len(regex_match.groups()) >= 2:
+            image_url = regex_match.group(2)
+            headers = {'referer': 'https://www.thefarside.com/'}
+            response = requests.get(image_url, headers=headers, verify=False)
+            if response.status_code == 200:
+                base64_bytes = base64.b64encode(response.content)
+                base64_string = base64_bytes.decode('utf-8')
+                image = "data:image/jpeg;base64,%s" % base64_string
+    return image
 
 def format_email(results, date_string):
     text_opening = 'Hi,\n\nHere are your daily comics for %s:\n' % date_string
@@ -39,7 +86,7 @@ def format_email(results, date_string):
     html_opening = '<html>\n\t<head>\n\t\t<title>Your Daily Comics</title>'
     html_opening += '\n\t\t<style>\n\t\t\tbody,html {background-color: #6b7b84; height: 100%; margin-top: 30px; margin-bottom: 30px; margin-left: 30px; margin-right: 30px; font-size: 125%;}'
     html_opening += '\n\t\t\t.mono {font-family: "Lucida Console", "Courier New", monospace;}'
-    html_opening += '\n\t\t\tp.pad {padding-bottom: 50px;}'
+    html_opening += '\n\t\t\tp.pad {padding-bottom: 50px; font-family: "Lucida Console", "Courier New", monospace;}'
     html_opening += '\n\t\t\th1,h2,h3 {color: black;}'
     html_opening += '\n\t\t\timg {max-width: 100%; height: auto; box-shadow: 5px 10px 18px #111111;}\n\t\t</style>'
     html_opening += '\n\t</head>\n\t<body>\n\t\t<basefont face = "courier">'
@@ -49,10 +96,19 @@ def format_email(results, date_string):
     text = text_opening
     html = html_opening
     for entry in results:
-        text += '\n\t- %s: %s' % (entry['name'], entry['image_url'])
+        if entry.get('image_url') is not None:
+            text += '\n\t- %s: %s' % (entry['name'], entry['image_url'])
+        else:
+            text += '\n\t- %s: No comic for today.' % entry['name']
         html += '\n\t\t<h2 class="mono">%s</h2>' % entry['name']
-        html += '\n\t\t\t<a href="%s">' % entry['gocomics_url']
-        html += '\n\t\t\t<img src="%s">' % entry['image_url']
+        if entry.get('image_url') is not None:
+            html += '\n\t\t\t<a href="%s">' % entry['comics_url']
+            html += '\n\t\t\t<img src="%s" alt="%s">' % (entry['image_url'], entry['name'])
+        elif entry['name'] == 'farside' and entry['image'] is not None:
+            html += '\n\t\t\t<a href="%s">' % FARSIDE_URL
+            html += '\n\t\t\t<img src="%s" alt="%s">' % (entry['image'], entry['name'])
+        else:
+            html += '\n\t\t\t<p class="pad"><b>No comic for today.</b>'
         html += '</a>'
     text += text_closing
     html += html_closing
@@ -70,12 +126,43 @@ def send_email(text, html, GMAIL_ID, GMAIL_PW, TARGET_EMAIL):
     message.attach(part2)
     # Create a secure SSL context
     context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", SSL_PORT, context=context) as server:
+    with smtplib.SMTP_SSL("smtp.gmail.com", EMAIL_SSL_PORT, context=context) as server:
         server.login(GMAIL_ID, GMAIL_PW)
         server.sendmail(
             GMAIL_ID, TARGET_EMAIL, message.as_string()
         )
         server.close()
+        print("Email sent")
+    return
+
+def process_comic(comic, today_date_string, results):
+    image_url = None
+    image = None
+    if comic['source'] == GOCOMICS:
+        url = "%s%s/%s" % (GOCOMICS_URL, comic['name'], today_date_string)
+        image_url = get_image_url(url, GOCOMICS_REGEX)
+    elif comic['source'] == COMICSKINGDOM:
+        url = "%s%s" % (COMICSKINGDOM_URL, comic['name'])
+        image_url = get_image_url(url, COMICSKINGDOM_REGEX)
+    elif comic['source'] == DILBERT:
+        url = DILBERT_URL
+        image_url = get_image_url(DILBERT_URL, DILBERT_REGEX)
+    elif comic['source'] == FARSIDE:
+        url = FARSIDE_URL
+        image_url = None
+        image = get_image(FARSIDE_URL, FARSIDE_REGEX)
+    else:
+        raise Exception('Invalid comic type.')
+
+    if image_url is not None:
+        new_entry = {'name': comic['name'], 'comics_url': url, 'image_url': image_url}
+        print(new_entry)
+    elif comic['source'] == FARSIDE and image is not None:
+        new_entry = {'name': comic['name'], 'comics_url': url, 'image_url': image_url, 'image': image}
+    else:
+        print('Comic %s not found for %s.' % (comic['name'], today_date_string))
+        new_entry = {'name': comic['name'], 'comics_url': url, 'image_url': image_url}
+    results.append(new_entry)
     return
 
 def main(cloud_function_input):
@@ -89,27 +176,24 @@ def main(cloud_function_input):
         TARGET_EMAIL = cloud_function_input.get('TARGET_EMAIL')
         if TARGET_EMAIL is None:
             raise Exception("Error no TARGET_EMAIL parameter defined.")
-        COMICS = cloud_function_input.get('COMICS')
-        if COMICS is None:
-            raise Exception("Error no COMICS parameter defined.")
-        else:
-            COMICS = json.loads(COMICS)
 
         now = datetime.now()
         today_date_string = now.strftime("%Y/%m/%d")
         processing_results = []
+
         threads = []
-        for comic_name in COMICS:
+        for comic in COMICS:
             th = Thread(target=process_comic,
-                        args=(comic_name, today_date_string, processing_results))
+                        args=(comic, today_date_string, processing_results))
             th.start()
             threads.append(th)
+
         for th in threads:
             th.join()
 
-        def sort_function(i):
-            return (COMICS.index(i['name']))
-
+        def sort_function(element):
+            indices = [i for i, c in enumerate(COMICS) if c['name'] == element['name']]
+            return (indices[0])
         processing_results.sort(key=sort_function)
 
         print(processing_results)
@@ -122,10 +206,11 @@ def main(cloud_function_input):
     except Exception as e:
         print('Error: ' + str(e))
         traceback.print_exc()
+        return {"statusCode": 500, "body": str(e)}
 
 if __name__ == '__main__':
+    print("Python %s" % sys.version)
     input = {"GMAIL_ID": os.environ['GMAIL_ID'],
              "GMAIL_PW": os.environ['GMAIL_PW'],
-             "TARGET_EMAIL": os.environ['TARGET_EMAIL'],
-             "COMICS": os.environ['COMICS']}
+             "TARGET_EMAIL": os.environ['TARGET_EMAIL']}
     main(input)
--- Cloud_Functions/Comics/deploy-comics.sh
+++ Cloud_Functions/Comics/deploy-comics.sh
@@ -6,11 +6,18 @@ ibmcloud fn namespace target Kellerman-Functions
 
 ibmcloud fn action update utils/Comics ./comics.py --kind python:3.7
 
-# Now list the package
-ibmcloud fn package get utils
+ibmcloud fn trigger update "Comics Daily" -p GMAIL_ID ${GMAIL_ID} -p GMAIL_PW ${GMAIL_PW} -p TARGET_EMAIL ${TARGET_EMAIL} --param cron "0 8 * * *"
+
+ibmcloud fn rule update comics_rule "Comics Daily" utils/Comics
 
 # Get the definition of the function
 ibmcloud fn action get utils/Comics
+
+# Get the definition of the trigger
+ibmcloud fn trigger get "Comics Daily"
+
+# Get the definition of the rule
+ibmcloud fn rule get comics_rule
 
 # invoke the function
 #
@@ -20,7 +27,7 @@ ibmcloud fn action get utils/Comics
 # Also, gmail password works locally but does not work from cloud function, one needs an app
 # password: https://support.google.com/accounts/answer/185833
 
-ibmcloud fn action invoke utils/Comics --blocking  -p GMAIL_ID ${GMAIL_ID} -p GMAIL_PW ${GMAIL_PW} -p TARGET_EMAIL ${TARGET_EMAIL} -p COMICS ${COMICS}
+ibmcloud fn action invoke utils/Comics --blocking  -p GMAIL_ID ${GMAIL_ID} -p GMAIL_PW ${GMAIL_PW} -p TARGET_EMAIL ${TARGET_EMAIL}
 
 # See the log results
 echo "Make sure the activation reported by the next command is the most recent one executed. Look for activation id"
